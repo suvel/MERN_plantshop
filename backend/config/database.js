968f5858305df9e3d8ed4ea36614990537efd1c4
Suvel <find.suvel@gmail.com>
2025-03-25 21:10:14 +0530

    chnages

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(con=>{
-        console.log(`MongoDB is connected to the host: ${con.connection.host} `)
-    })
-}
+const connectDatabase = () => {
+  return new Promise(async (resolve, reject) => {
+    await mongoose
+      .connect(process.env.DB_LOCAL_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((con) => {
+        console.log(
+          `MongoDB is connected to the host: ${con.connection.host} `
+        );
+        resolve();
+      })
+      .catch((exception) => {
+        reject(exception);
+      });
+  });
+};
 
 module.exports = connectDatabase;
--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,21 +1,30 @@
-const products = require('../data/products.json');
-const Product = require('../models/productModel');
-const dotenv = require('dotenv');
-const connectDatabase = require('../config/database')
+const products = require("../data/products.json");
+const Product = require("../models/productModel");
+const dotenv = require("dotenv");
+const connectDatabase = require("../config/database");
 
-dotenv.config({path:'backend/config/config.env'});
-connectDatabase();
+dotenv.config({ path: "backend/config/config.env" });
 
-const seedProducts = async ()=>{
-    try{
-        await Product.deleteMany();
-        console.log('Products deleted!')
-        await Product.insertMany(products);
-        console.log('All products added!');
-    }catch(error){
-        console.log(error.message);
-    }
-    process.exit();
-}
+const seedProducts = async () => {
+  try {
+    await Product.deleteMany();
+    console.log("Products deleted!");
+    await Product.insertMany(products);
+    console.log("All products added!");
+  } catch (error) {
+    console.log(error.message);
+  }
+  process.exit();
+};
 
-seedProducts();
+const start = async () => {
+  try {
+    await connectDatabase();
+    await seedProducts();
+  } catch (exception) {
+    console.log("ERROR @ start");
+    console.log(exception);
+  }
+};
+
+start();
